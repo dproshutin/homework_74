0be5ea10dd46ae11bd519ee0c8b9c98dad0b7f0f
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-12-15 16:34:50 +0600

    Реализован POST-запрос на путь /messages. Данные, передаваемые в JSON-теле запроса, сохраняются в новый файл, который имеет название в зависимости от времени запроса.

--- app/messages.js
+++ app/messages.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
+
+
+router.post("/", (req, res) => {
+    const message = req.body;
+    const date = new Date();
+    const datetime = date.toISOString();
+    const fileName = "messages/" + datetime + ".txt";
+    const data = {
+        ...message,
+        datetime
+    };
+    fs.writeFileSync(fileName, JSON.stringify(data));
+    res.send(data);
+});
+
 module.exports = router;
