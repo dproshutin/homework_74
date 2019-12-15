bfc87c6042919ce1d5b2c32173e228caff6827d7
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-12-15 18:58:44 +0600

    Реализован GET запрос на получение данных в массив из последних пяти файлов, загруженных в директорию messages.

--- app/messages.js
+++ app/messages.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
+const NUMBER_OF_LAST_MESSAGES = 5;
 
 
 router.post("/", (req, res) => {
@@ -14,6 +15,22 @@ router.post("/", (req, res) => {
     };
     fs.writeFileSync(fileName, JSON.stringify(data));
     res.send(data);
+});
+
+router.get("/", (req, res) => {
+    fs.readdir("messages/", (err, files) => {
+        if (err) {
+            throw new Error(err);
+        }
+        let messages = files.slice(-NUMBER_OF_LAST_MESSAGES).map(file => {
+            let item = fs.readFileSync("messages/" + file);
+            // let item = fs.readFile("messages/" + file, (err, data) => {
+            //     return data;
+            // });
+            return JSON.parse(String(item));
+        });
+        return res.send(messages);
+    });
 });
 
 module.exports = router;
